c85045ee5b7144e6664d861b4ccadb257e391bcc
gabriel <823126028@qq.com>
2020-06-17 16:26:29 +0800

    haha

---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String[] args) {
+        while (true) {
+            try {
+                System.out.println("running");
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
