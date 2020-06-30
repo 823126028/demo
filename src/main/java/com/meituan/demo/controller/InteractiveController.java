336d8c90edad8c75521e6df7e048aedfff132ca5
gabriel <823126028@qq.com>
2020-06-30 19:28:07 +0800

    提交logback-spring.xml

--- src/main/java/com/meituan/demo/controller/InteractiveController.java
+++ src/main/java/com/meituan/demo/controller/InteractiveController.java
@@ -1,16 +1,23 @@
 package com.meituan.demo.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InteractiveController {
+
+    private static final Logger logger = LoggerFactory.getLogger(InteractiveController.class);
     @RequestMapping("/")
     public String sayHello() {
+        logger.info("hello, mcloud");
         return "hello, mcloud";
     }
     @RequestMapping("/version")
     public String version() {
+       logger.info("mcloud:0.0.1");
         return "mcloud:0.0.1";
     }
 }
