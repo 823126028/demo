cbebf5213b7605f20500d51b99ac0c3063ee6a4f
陈建亮 <chenjianliang03@meituan.com>
2020-07-03 11:47:22 +0800

    提交包含错误日志的

--- src/main/java/com/meituan/demo/controller/InteractiveController.java
+++ src/main/java/com/meituan/demo/controller/InteractiveController.java
@@ -6,18 +6,31 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class InteractiveController {
 
     private static final Logger logger = LoggerFactory.getLogger(InteractiveController.class);
     @RequestMapping("/")
-    public String sayHello() {
-        logger.info("hello, mcloud");
+    public String sayHello() throws UnknownHostException {
+        logger.info("hello, mcloud:" + getHost());
         return "hello, mcloud";
     }
     @RequestMapping("/version")
-    public String version() {
-       logger.info("mcloud:0.0.1");
+    public String version() throws UnknownHostException {
+       logger.info("mcloud:0.0.1" + getHost());
         return "mcloud:0.0.1";
+    }
+    @RequestMapping("/errors")
+    public String errors() throws UnknownHostException {
+        logger.info("visit error" + getHost());
+        return "mcloud:error:0.0.1";
+    }
+
+    private static String getHost() throws UnknownHostException {
+        InetAddress inetAddress = InetAddress.getLocalHost();
+        return inetAddress.getHostName();
     }
 }
