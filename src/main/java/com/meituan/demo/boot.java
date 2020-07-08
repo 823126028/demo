f2042c423b9c220a7faada458c38bd9a600ae5e3
陈建亮 <chenjianliang03@meituan.com>
2020-07-08 20:21:02 +0800

    提交resource 333修改

--- src/main/java/com/meituan/demo/boot.java
+++ src/main/java/com/meituan/demo/boot.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class boot {
         public static void main(String[] args) {
+            //123
             SpringApplication.run(boot.class, args);
         }
 }
